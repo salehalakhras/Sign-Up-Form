f8df0f1beaaa8d2253a397a46de1d4d32f1ab3ff
Saleh A <salehalakhras2000@gmail.com>
2022-10-02 02:48:57 +0300

    redesigned the page with tailwind

---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}"
+    ],
+    theme: {
+        backgroundImage: {
+            'backg': "url(theo-vLoexGvJ20k-unsplash.jpg)"
+        },
+        extend: {},
+    },
+    plugins: [],
+}
